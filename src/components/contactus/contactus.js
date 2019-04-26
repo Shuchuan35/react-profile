1655f8f25a712c79b3fdd568653f9e1efe474a11
scream <hou.jennifer35@gmail.com>
2019-04-25 22:23:04 -0400

    added form validation and display spinning image when submitting form

--- src/components/contactus/contactus.js
+++ src/components/contactus/contactus.js
@@ -6,7 +6,8 @@ export default class Contactus extends Component {
     name: '',
     email: '',
     subject: '',
-    message: ''
+    message: '',
+    isImageLoader: false
   }
 
   handleChange = e => {
@@ -17,12 +18,16 @@ export default class Contactus extends Component {
 
   formSubmit = e => {
     e.preventDefault();
+    if (!e.target.checkValidity()) {
+      return;
+    }
     const params = {
       from_name: this.state.name,
       from_email: this.state.email,
       subject: this.state.subject,
       message: this.state.message
     }
+    this.setState({ isImageLoader: true });
     console.log(this.state.name);
     console.log(this.state.email);
     console.log(this.state.subject);
@@ -30,7 +35,7 @@ export default class Contactus extends Component {
     // emailjs.send('gmail', 'formsubmit', params, 'user_ISBvizalqrfttKA25yz7y')
     //   .then((response) => {
     //     console.log('SUCCESS!', response.status, response.text);
-    //     this.setState({ name: '', email: '', subject: '', message: '' });
+    //     this.setState({ name: '', email: '', subject: '', message: '', isImageLoader: false });
     //   }, (err) => {
     //     console.log('FAILED...', err);
 
@@ -52,41 +57,21 @@ export default class Contactus extends Component {
                 <input onChange={this.handleChange} name="email" type="email" placeholder="your@email.com" required value={this.state.email} />
 
                 <label htmlFor="message-subject">Subject <span className="required">*</span></label>
-                <input onChange={this.handleChange} name="subject"  type="text" placeholder="Subject" required value={this.state.subject} />
+                <input onChange={this.handleChange} name="subject" type="text" placeholder="Subject" required value={this.state.subject} />
 
                 <label htmlFor="message-input">Message <span className="required">*</span></label>
                 <textarea onChange={this.handleChange} name="message" type="text" placeholder="Please write your message here" required value={this.state.message} />
 
                 <div className="button--container">
                   <button type="submit" className="submit">Submit</button>
+                  
+                  {this.state.isImageLoader &&
+                    <div style={{ display: 'inline-block' }}>
+                      <img alt="submitting" src="images/loader.gif" />
+                    </div>
+                  }
                 </div>
               </form>
-              {/* <form id="contactForm" name="contactForm">
-                <fieldset>
-                  <div>
-                    <label htmlFor="contactName">Name <span className="required">*</span></label>
-                    <input type="text" id="contactName" name="contactName" />
-                  </div>
-                  <div>
-                    <label htmlFor="contactEmail">Email <span className="required">*</span></label>
-                    <input type="text" id="contactEmail" name="contactEmail" />
-                  </div>
-                  <div>
-                    <label htmlFor="contactSubject">Subject</label>
-                    <input type="text" id="contactSubject" name="contactSubject" />
-                  </div>
-                  <div>
-                    <label htmlFor="contactMessage">Message <span className="required">*</span></label>
-                    <textarea id="contactMessage" name="contactMessage" />
-                  </div>
-                  <div>
-                    <button className="submit"  >Submit</button>
-                    <span id="image-loader">
-                      <img alt="loading" src="images/loader.gif" />
-                    </span>
-                  </div>
-                </fieldset>
-              </form>  */}
               {/* contact-warning */}
               <div id="message-warning"> Error boy</div>
               {/* contact-success */}
