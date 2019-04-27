b4bfd735b2f7cfe13014edc70632ee245f249ff8
scream <hou.jennifer35@gmail.com>
2019-04-27 15:19:54 -0400

    Updates

--- precache-manifest.68533626f5c4a4b3809f1313fadd1df6.js
+++ precache-manifest.68533626f5c4a4b3809f1313fadd1df6.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/react-profile/static/js/runtime~main.328ac560.js"
   },
   {
-    "revision": "c5fdbce0c32b07a72b53",
-    "url": "/react-profile/static/js/main.7a42bb53.chunk.js"
+    "revision": "f4f6bd12fb6dc7df2911",
+    "url": "/react-profile/static/js/main.b9fb0189.chunk.js"
   },
   {
     "revision": "a66da7dcefa3888a0ed9",
     "url": "/react-profile/static/js/2.ab413707.chunk.js"
   },
   {
-    "revision": "c5fdbce0c32b07a72b53",
+    "revision": "f4f6bd12fb6dc7df2911",
     "url": "/react-profile/static/css/main.042847b8.chunk.css"
   },
   {
-    "revision": "b10d73df09af25d846853a042b4f8398",
+    "revision": "8eaeef9bc9efee4119781af34acf3154",
     "url": "/react-profile/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-profile/precache-manifest.b700ba8c211064e2587870f12bf16df0.js"
+  "/react-profile/precache-manifest.68533626f5c4a4b3809f1313fadd1df6.js"
 );
 
 workbox.clientsClaim();
